2c71f19a7134de02e4958ffd6e5fa30310cb5f56
Alton Michaux <alton.michaux@yahoo.com>
2022-10-03 22:55:59 -0400

    commented out a rubocop complaint

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@
 
 Rails.application.routes.draw do
   devise_for :accounts
-  
-  root to: "movies#index"
+
+  root to: 'movies#index'
 end
--- test/controllers/podcasts_controller_test.rb
+++ test/controllers/podcasts_controller_test.rb
@@ -1,7 +1,9 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class PodcastsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get podcasts_index_url
     assert_response :success
   end
